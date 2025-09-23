d4d97dbfa3cdb953179831087e8ff48f0e392151
Reinis Grinbergs <rpg600@gmail.com>
2025-09-23 17:57:58 +0200

    Update compile_yolo11n.py

--- steps/3_process/compile_yolo11n.py
+++ steps/3_process/compile_yolo11n.py
@@ -1,7 +1,7 @@
 from hailo_sdk_client import ClientRunner
 
 model_name = 'best'
-quantized_model_har_path = f'/home/sam/Hailo8l/model/runs/detect/retrain_yolov11n/weights/{model_name}_quantized_model.har'
+quantized_model_har_path = f'/root/runs/detect/train5/weights/{model_name}_quantized_model.har'
 
 runner = ClientRunner(har=quantized_model_har_path)
 
